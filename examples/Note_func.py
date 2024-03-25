b463dd404cf62adf00bad6ef485b3cc0cb1f5a09
Anizet <arnaudnizet@hotmail.com>
2024-03-25 19:21:27 +0100

    Improved on BFGS and BO trajectories, started implementing GPU acceleration for BO

--- examples/Note_func.py
+++ examples/Note_func.py
@@ -1,8 +1,13 @@
 import numpy as np
 from ase.build import bulk
 import pickle
+import torch
+
 with open('atoms.pkl', 'rb') as f:
     atoms = pickle.load(f)
+
+# Check if CUDA is available and set PyTorch device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 input = {}
 with open('Input.txt', 'r') as file:
@@ -21,10 +26,10 @@ with open('Input.txt', 'r') as file:
 # ## Set up evaluation function (pipe to ASE) for trial parameters suggested by Ax. 
 # Note that this function can return additional keys that can be used in the `outcome_constraints` of the experiment.
 def evaluate(parameters):
-    x = np.array([parameters.get(f"x"), parameters.get(f"y"), parameters.get(f"z")])
+    x = torch.tensor([parameters.get(f"x"), parameters.get(f"y"), parameters.get(f"z")], device = device)
     # Can put zeros since constraints are respected by set_positions.
-    new_pos = np.vstack([np.zeros((atoms.get_number_of_atoms() - 1, 3)), x])
-    atoms.set_positions(new_pos, apply_constraint=True)
+    new_pos = torch.vstack([torch.zeros((atoms.get_number_of_atoms() - 1, 3), device = device), x])
+    atoms.set_positions(new_pos.cpu().numpy(), apply_constraint=True)
     energy = atoms.get_potential_energy()
     
     # In our case, standard error is 0, since we are computing a synthetic function.
@@ -59,18 +64,17 @@ model = BoTorchModel(
         mll_class=ExactMarginalLogLikelihood,
         # Optional, dictionary of keyword arguments to underlying
         # BoTorch `Model` constructor
-        model_options={},
+        model_options={"torch_device": device},
     ),
     # Optional BoTorch `AcquisitionFunction` to use instead of default
     botorch_acqf_class=str_to_class(input['bo_acquisition_f']),
     # Optional dict of keyword arguments, passed to the input
     # constructor for the given BoTorch `AcquisitionFunction`
-    acquisition_options={},
+    acquisition_options={"torch_device": device},
 )
 
 # ## Create client and initial sampling strategy to warm-up the GP model
 
-import torch
 from ax.modelbridge.generation_strategy import GenerationStep, GenerationStrategy
 from ax.modelbridge.registry import Models
 
@@ -84,7 +88,7 @@ gs = GenerationStrategy(
             num_trials=input['gs_init_steps'],  # How many trials should be produced from this generation step
             min_trials_observed=3,  # How many trials need to be completed to move to next model
             max_parallelism=5,  # Max parallelism for this step
-            #model_kwargs={"seed": 999},  # Any kwargs you want passed into the model
+            #model_kwargs={},  # Any kwargs you want passed into the model
             #model_gen_kwargs={},  # Any kwargs you want passed to `modelbridge.gen`
         ),
         # 2. Bayesian optimization step (requires data obtained from previous phase and learns
@@ -92,7 +96,10 @@ gs = GenerationStrategy(
         GenerationStep(
             model=Models.BOTORCH_MODULAR,
             num_trials=-1,  # No limitation on how many trials should be produced from this step
-            max_parallelism=3,  # Parallelism limit for this step, often lower than for Sobol
+            max_parallelism=3,
+            model_kwargs={"torch_device": device},  # Any kwargs you want passed into the model
+            model_gen_kwargs={"torch_device": device},
+            # Parallelism limit for this step, often lower than for Sobol
             # More on parallelism vs. required samples in BayesOpt:
             # https://ax.dev/docs/bayesopt.html#tradeoff-between-parallelism-and-total-number-of-trials
         ),
