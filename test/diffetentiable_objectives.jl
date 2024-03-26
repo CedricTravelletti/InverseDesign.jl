66905aa6ad0b71a4d0cbc2e9a8f8d01373879415
CedricTravelletti <cedrictravelletti@gmail.com>
2024-03-26 11:50:16 +0100

    Added (failing) test for band gap derivative.

--- test/diffetentiable_objectives.jl
+++ test/diffetentiable_objectives.jl
@@ -24,3 +24,28 @@
     
     @test isapprox(norm(dfx0 - dfx0_finite), 0; atol=1e-3)
 end
+
+@testitem "Test direct band gap derivative wrt position." begin
+    using InverseDesign
+    using DFTK
+    using ForwardDiff
+    using FiniteDiff
+    using LinearAlgebra
+    
+    system = construct_silicon()
+    
+    # Create a simple calculator for the model.
+    model_kwargs = (; functionals = [:lda_x, :lda_c_pw], temperature = 1e-4)
+    basis_kwargs = (; kgrid = [4, 4, 4], Ecut = 30.0)
+    scf_kwargs = (; tol = 1e-5)
+    calculator = DFTKCalculator(; model_kwargs, basis_kwargs, scf_kwargs, verbose=true)
+    
+    # Compute energy at equilibrium position.
+    # Note that we have to make x0 mutable.
+    x0 = Vector(flatten(DFTK.parse_system(system).positions))
+    
+    dfx0 = ForwardDiff.gradient(x -> direct_band_gap(calculator, system, x), x0)
+    dfx0_finite = FiniteDiff.finite_difference_gradient(x -> direct_band_gap(calculator, system, x), x0)
+    
+    @test isapprox(norm(dfx0 - dfx0_finite), 0; atol=1e-3)
+end
