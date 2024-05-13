4d713a7720d663db68c34bb89f75c6068baa472d
CedricTravelletti <cedrictravelletti@gmail.com>
2024-05-13 14:48:37 +0200

    Fixed conflicts

--- src/differentiable_objectives.jl
+++ src/differentiable_objectives.jl
@@ -24,18 +24,47 @@ function unflatten(vector)
 end
 
 """
+Update the positions of a DFTK system. Reduced coordinates version (unitless).
+
+Returns:
+- New positions, in reduced coordinates.
+
+"""
+function _update_positions(positions_flat::AbstractVector{<:Real}, model,
+			   deformation_tensor=LinearAlgebra.I)
+    unflatten(positions_flat)
+end
+
+"""
+Update the positions of a DFTK system. Cartesian version (unitful).
+
+Returns:
+- New positions, in reduced coordinates.
+
+"""
+function _update_positions(positions_flat::AbstractVector{<:Unitful.Quantity}, model,
+			   deformation_tensor=LinearAlgebra.I)
+    positions = unflatten(positions_flat)
+    # First update to new positions, then apply deformation.
+    positions = [collect((x for x in deformation_tensor * position))
+		 for position in positions]
+    DFTK.vector_cart_to_red.(model, positions)
+end
+
+"""
 Computes dual of the SCF by updating system positions in a differentiable way.
 
 Arguments:
-- `positions` flattened vector of atomic positions in fractional coordinates.
+- `positions` flattened vector of atomic positions in cartesian coordinates.
 
 """
 function compute_scf_dual(calculator::DFTKCalculator,
-		               system::AbstractSystem, positions)
-    positions = unflatten(positions)
+		               system::AbstractSystem, positions_flat)
     # Original model. Symmetries disabled for differentiability.
     model = model_DFT(system; symmetries=false, calculator.params.model_kwargs...)
-    new_model = Model(model; positions)
+    new_positions = _update_positions(positions_flat,model)
+    
+    new_model = Model(model; positions=new_positions)
     new_basis = PlaneWaveBasis(new_model; calculator.params.basis_kwargs...)
     self_consistent_field(new_basis; calculator.params.scf_kwargs...)
 end
@@ -45,19 +74,23 @@ Computes dual of the SCF by updating system positions in a differentiable way.
 
 Arguments:
 - `positions` ComponenVector with components `atoms` containing the (flattened) 
-              vector of atomic positions in fractional coordinates and component 
-	      `strain` containing the strain to apply (in voigt notation).
+              vector of atomic positions component 
+	      `strain` containing the strain to apply (in voigt notation). 
+	      If the atomic positions have units, they are assumed to refer to cartesian 
+	      positions; if they are unitless, they are assumed to refer to fractional positions.
 
 """
 function compute_scf_dual(calculator::DFTKCalculator,
-		               system::AbstractSystem, positions::ComponentVector)
-    deformation_tensor = I + GeometryOptimization.voigt_to_full(positions.strain)
-    positions = [collect((x for x in deformation_tensor * position))
-		 for position in unflatten(positions.atoms)]
-    lattice = collect.(deformation_tensor * bbox_to_matrix(bounding_box(system)))
+		               system::AbstractSystem, positions_flat::ComponentVector)
+    deformation_tensor = I + voigt_to_full(positions_flat.strain)
     # Original model. Symmetries disabled for differentiability.
     model = model_DFT(system; symmetries=false, calculator.params.model_kwargs...)
-    new_model = Model(model; positions, lattice)
+    new_positions = _update_positions(positions_flat.atoms, model, deformation_tensor)
+
+    # DFTK uses lattices in natural units.
+    lattice = deformation_tensor * austrip.(bbox_to_matrix(bounding_box(system)))
+    
+    new_model = Model(model; positions=new_positions, lattice)
     new_basis = PlaneWaveBasis(new_model; calculator.params.basis_kwargs...)
     self_consistent_field(new_basis; calculator.params.scf_kwargs...)
 end
@@ -105,11 +138,6 @@ Arguments:
 
 """
 function gamma_point_bandgap(calculator::DFTKCalculator, system::AbstractSystem, positions_flat)
-    scfres_dual = compute_scf_dual(calculator, system, positions_flat)
-    vi = valence_band_index(scfres_dual)
-    scfres_dual.eigenvalues[1][vi + 1] - scfres_dual.eigenvalues[1][vi]
-end
-function gamma_point_bandgap(calculator::DFTKCalculator, system::AbstractSystem, positions_flat::ComponentVector)
     scfres_dual = compute_scf_dual(calculator, system, positions_flat)
     vi = valence_band_index(scfres_dual)
     scfres_dual.eigenvalues[1][vi + 1] - scfres_dual.eigenvalues[1][vi]
