4ebb91eeb3b6e7141f825639c28945fff66e54ea
CedricTravelletti <cedrictravelletti@gmail.com>
2024-05-17 09:51:37 +0200

    Updated run scripts Works fine now.

--- reporting/optimization/gammaP_bandgap_on_grid.jl
+++ reporting/optimization/gammaP_bandgap_on_grid.jl
@@ -2,22 +2,14 @@
 This is used for later verifications.
 
 """
+
 using StaticArrays
 using JLD2
 using InverseDesign
 using DFTK
 
 
-system = construct_diamond()
-
-model_kwargs = (; functionals = [:lda_x, :lda_c_pw], temperature = 1e-4)
-basis_kwargs = (; kgrid = [5, 5, 5], Ecut = 40.0)
-scf_kwargs = (; tol = 1e-7)
-calculator = DFTKCalculator(; model_kwargs, basis_kwargs, scf_kwargs, verbose=true)
-
-# Compute energy at equilibrium position.
-# Note that we have to make x0 mutable.
-x0 = Vector(flatten(DFTK.parse_system(system).positions))
+gammaP_bandgap_vs_strain12 = construct_gammaP_bandgap_vs_strain12()
 
 # Grid of strains along x-y.
 strain_grid = -0.1 : 0.01 : 0.1
@@ -26,9 +18,9 @@ bandgaps = zeros(MVector{length(iterator)})
 bandgaps_grad = zeros(MMatrix{length(iterator), 2})
 
 for (i, x) in enumerate(iterator)
-	res = InverseDesign.gamma_point_bandgap_vs_strain12(collect(x))
+	res = gammaP_bandgap_vs_strain12(collect(x))
 	bandgaps[i] = res[1]
-	bandgaps_grad[i, :] = res[2:]
+	bandgaps_grad[i, :] = res[2:end]
 	if i % 10 == 0
 	    save_object("./data/bandgaps.jld2", bandgaps)
 	    save_object("./data/bandgaps_grad.jld2", bandgaps_grad)
--- src/InverseDesign.jl
+++ src/InverseDesign.jl
@@ -7,7 +7,7 @@ using DFTK
 export flatten, unflatten, energy_wrt_pos, direct_bandgap, gamma_point_bandgap
 include("differentiable_objectives.jl")
 
-export construct_silicon, construct_diamond
+export construct_silicon, construct_diamond, construct_gammaP_bandgap_vs_strain12
 include("example_systems.jl")
 
 end
